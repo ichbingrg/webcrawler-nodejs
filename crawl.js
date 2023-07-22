7acc0f5ee7a7df384cd3b315c8b5e42ea7ab1f47
sudip <gsudip97+git@gmail.com>
2023-07-22 19:02:40 +0200

    crawl single page with error handling

--- crawl.js
+++ crawl.js
@@ -1,5 +1,34 @@
 const {JSDOM} = require('jsdom');
 
+const crawlPage = async (currentURL) =>  {
+    console.log(`actively crawling ${currentURL}`)
+        
+    
+    // if user gives bad link
+    try{
+        const resp = await fetch(currentURL)
+
+        // check for status 200
+        if(resp.status > 399){
+            console.log(`error in fetch with status code: ${resp.status} on page: ${currentURL}`)
+            return
+        }
+        
+        // make sure the response is html
+        const contentType = resp.headers.get("content-type")
+        if(!contentType.includes("text/html")){
+            console.log(`error in fetch with non html response, content type : ${contentType} on page: ${currentURL}`)
+            return
+        }
+
+        console.log(await resp.text())
+    }catch(e){
+        console.log(`Error in fetch : ${e.message}, on page : ${currentURL}`);
+    }
+
+}
+
+
 const getURLsFromHTML = (htmlBody, baseURL) =>{
     const urls = [];
     const dom =  new JSDOM(htmlBody)
@@ -52,5 +81,6 @@ const normalizeURL = (urlString) =>{
 
 module.exports={
     normalizeURL,
-    getURLsFromHTML
+    getURLsFromHTML,
+    crawlPage
 }
--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-console.log("Hello");
+
+const {crawlPage} = require('./crawl')
+
+
+const main = () => {
+    if (process.argv.length < 3){
+        // process.argv = [<node>, <main.js>, <any argument provided during npm start>]
+        console.log("no website provided");
+        process.exit(1)
+    }
+    if (process.argv.length > 3){
+        // process.argv = [<node>, <main.js>, <any argument provided during npm start>]
+        console.log("too many command line arguments");
+        process.exit(1)
+    }
+
+    const baseURL = process.argv[2];
+
+    console.log("starting crawl of " + baseURL + "    ...");
+    crawlPage(baseURL)
+}
+
+main()
